c6336807a3177278b2bff30602782c05de031938
Hafizjunaid <Hafizjunaid971@gmail.com>
2021-03-28 20:52:46 +0500

    first commit

---
+++ Controllers/AcountController.cs
@@ -0,0 +1,58 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using techsolution.Models;
+using System.Web.Security;
+
+namespace techsolution.Controllers
+{
+    [AllowAnonymous]
+    public class AcountController : Controller
+    {
+        // GET: Acount
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Models.Membership Models)
+        {
+            using (var context = new Tech_EmpireEntities())
+            {
+                bool isValid = context.User.Any(x => x.UserName == Models.UserName && x.Password == Models.Password);
+
+                if (isValid)
+                {
+                    FormsAuthentication.SetAuthCookie(Models.UserName, false);
+                    return RedirectToAction("Index", "Employees");
+                }
+                ModelState.AddModelError("", "Invalid user name and password");
+                       return View();
+            }
+
+        }
+
+        public ActionResult Signup()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Signup(User Models)
+        {
+            using (var context = new Tech_EmpireEntities())
+            {
+                context.User.Add(Models);
+                context.SaveChanges();
+            }
+            return RedirectToAction("Login");
+
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+    }
+}
